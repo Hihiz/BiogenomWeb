da5395833f671dfb55637346d3ca6b69ea184f16
Hihiz <public@mail.ru>
2025-07-14 02:26:41 +0300

    Добавить класс контекста

---
+++ BiogenomWeb.Infrastructure/Data/ApplicationDbContext.cs
@@ -0,0 +1,28 @@
+﻿using BiogenomWeb.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace BiogenomWeb.Infrastructure.Data
+{
+    /// <summary>
+    /// Класс контекста EF Core.
+    /// </summary>
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext()
+        {
+        }
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<RecomendationProductEntity> RecomendationProducts { get; set; }
+
+        public DbSet<ReportEntity> Reports { get; set; }
+
+        public DbSet<VitaminEntity> Vitamins { get; set; }
+
+        public DbSet<VitaminReportEntity> VitaminReports { get; set; }     
+    }
+}
