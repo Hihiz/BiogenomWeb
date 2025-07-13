71a87bb217c72049d633ad1718926a2091dfb161
Hihiz <public@mail.ru>
2025-07-14 02:25:52 +0300

    Добавить классы сущностей

---
+++ BiogenomWeb.Domain/Entities/VitaminEntity.cs
@@ -0,0 +1,17 @@
+﻿namespace BiogenomWeb.Domain.Entities;
+
+/// <summary>
+/// Таблица витаминов/минералов.
+/// </summary>
+public class VitaminEntity
+{
+    /// <summary>
+    /// PK.
+    /// </summary>
+    public int Id { get; set; }
+
+    /// <summary>
+    /// Название витамина/минерала.
+    /// </summary>
+    public string Title { get; set; } = null!;
+}
