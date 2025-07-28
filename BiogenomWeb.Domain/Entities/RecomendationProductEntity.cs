2b87821ca59fb0a21ae4218a6524a36a41f53b7d
Hihiz <public@mail.ru>
2025-07-28 23:34:12 +0300

    Добавить сущность персонализированных наборов

---
+++ BiogenomWeb.Domain/Entities/RecomendationProductEntity.cs
@@ -0,0 +1,22 @@
+﻿namespace BiogenomWeb.Domain.Entities;
+
+/// <summary>
+/// Таблица рекомендаций (персонализированных наборов) БАДов для пользователя.
+/// </summary>
+public class RecomendationProductEntity
+{
+    /// <summary>
+    /// PK.
+    /// </summary>
+    public int Id { get; set; }
+
+    /// <summary>
+    /// Название набора (БАДа).
+    /// </summary>
+    public string Title { get; set; } = null!;
+
+    /// <summary>
+    /// Путь до картинки набора (БАДа).
+    /// </summary>
+    public string? ImageUrl { get; set; }
+}
