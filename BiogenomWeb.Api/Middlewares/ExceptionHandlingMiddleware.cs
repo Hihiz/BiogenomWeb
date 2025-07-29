47c42da3f7cea9e7c2219e5b819ea20c5cd17e77
Hihiz <public@mail.ru>
2025-07-29 23:50:39 +0300

    Добавить метод перехвата запроса

--- BiogenomWeb.Api/Middlewares/ExceptionHandlingMiddleware.cs
+++ BiogenomWeb.Api/Middlewares/ExceptionHandlingMiddleware.cs
@@ -1,7 +1,7 @@
 ﻿namespace BiogenomWeb.Api.Middlewares
 {
     /// <summary>
-    /// Класс прехвата и обработки ошибок.
+    /// Класс прехвата и обработки исключений.
     /// </summary>
     public class ExceptionHandlingMiddleware
     {
@@ -13,11 +13,29 @@
         /// </summary>
         /// <param name="next">Запрос.</param>
         /// <param name="logger">Логгер.</param>
-        public ExceptionHandlingMiddleware(RequestDelegate next, 
+        public ExceptionHandlingMiddleware(RequestDelegate next,
             ILogger<ExceptionHandlingMiddleware> logger)
         {
             _next = next;
             _logger = logger;
+        }
+
+        /// <summary>
+        /// Метод перехвата запроса.
+        /// </summary>
+        /// <param name="context">Контекст запроса.</param>
+        public async Task InvokeAsync(HttpContext context)
+        {
+            try
+            {
+                await _next(context);
+            }
+
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, ex.Message);
+                await HandleExceptionAsync(context, ex);
+            }
         }
     }
 }
