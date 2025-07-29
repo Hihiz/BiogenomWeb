e778efe92cd74e68320633229b065f6a94927ac1
Hihiz <public@mail.ru>
2025-07-29 21:24:34 +0300

    Добавить класс перехвата ошибок

---
+++ BiogenomWeb.Api/Middlewares/ExceptionHandlingMiddleware.cs
@@ -0,0 +1,23 @@
+﻿namespace BiogenomWeb.Api.Middlewares
+{
+    /// <summary>
+    /// Класс прехвата и обработки ошибок.
+    /// </summary>
+    public class ExceptionHandlingMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly ILogger<ExceptionHandlingMiddleware> _logger;
+
+        /// <summary>
+        /// Конструктор.
+        /// </summary>
+        /// <param name="next">Запрос.</param>
+        /// <param name="logger">Логгер.</param>
+        public ExceptionHandlingMiddleware(RequestDelegate next, 
+            ILogger<ExceptionHandlingMiddleware> logger)
+        {
+            _next = next;
+            _logger = logger;
+        }
+    }
+}
