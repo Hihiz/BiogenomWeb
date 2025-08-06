4be984e0963b762d0ec2dc1bb4d13cd302ac6638
Hihiz <public@mail.ru>
2025-08-07 01:32:47 +0300

    Добавить тест для получения списка

---
+++ BiogenomWeb.Tests/Unit/Service/Vitamin/GetVitaminsTest.cs
@@ -0,0 +1,22 @@
+﻿using Moq;
+
+namespace BiogenomWeb.Tests.Unit.Service.Vitamin
+{
+    public class GetVitaminsTest : BaseUnitTest
+    {
+        [Fact]
+        public async Task GetVitaminsAsyncTest()
+        {
+            // Arrange
+            MockVitaminRepository
+               .Setup(repo => repo.GetVitaminsAsync())
+               .ReturnsAsync(BaseVitaminEntities);
+
+            // Act
+            var result = await VitaminService.GetVitaminsAsync();
+
+            // Assert
+            Assert.NotNull(result);
+        }
+    }
+}
