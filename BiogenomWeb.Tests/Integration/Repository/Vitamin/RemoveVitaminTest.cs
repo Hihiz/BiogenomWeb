a00265f83ff74a367433a02d37111604696c9337
Hihiz <public@mail.ru>
2025-08-07 01:35:42 +0300

    Добавить тест удаления витамина

---
+++ BiogenomWeb.Tests/Integration/Repository/Vitamin/RemoveVitaminTest.cs
@@ -0,0 +1,29 @@
+﻿namespace BiogenomWeb.Tests.Integration.Repository.Vitamin;
+
+public class RemoveVitaminTest : IClassFixture<BaseTestContainersIntegration>
+{
+    private readonly BaseTestContainersIntegration _integration;
+
+    public RemoveVitaminTest(BaseTestContainersIntegration integration)
+    {
+        _integration = integration;
+    }
+
+    [Fact]
+    public async Task RemoveVitaminAsyncTest()
+    {
+        // Arrange
+        await _integration.VitaminRepository.CreateVitaminAsync("Create Vitamin");
+
+        // Act & Assert
+        await _integration.VitaminRepository!.RemoveVitaminAsync(1);
+    }
+
+    [Fact]
+    public async Task RemoveVitaminAsyncIdZeroTest()
+    {
+        await Assert.ThrowsAsync<InvalidOperationException>(() => 
+            _integration.VitaminRepository!.RemoveVitaminAsync(0));
+    }
+}
+
