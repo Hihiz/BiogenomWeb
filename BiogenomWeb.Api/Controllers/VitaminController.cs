22cc4f57517fff8a35c6a6d5f93512189f3755b5
Hihiz <public@mail.ru>
2025-08-06 19:13:20 +0300

    Добавить контроллер витаминов

---
+++ BiogenomWeb.Api/Controllers/VitaminController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BiogenomWeb.Api.Controllers
+{
+    /// <summary>
+    /// Контроллер витаминов.
+    /// </summary>
+    [Route("api/[controller]")]
+    [ApiController]
+    public class VitaminController : ControllerBase
+    {
+    }
+}
