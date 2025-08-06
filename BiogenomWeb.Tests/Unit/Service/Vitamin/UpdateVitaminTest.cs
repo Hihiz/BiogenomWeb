9b66903a29f1930c452965071386b93d3c8f3a53
Hihiz <public@mail.ru>
2025-08-07 01:33:14 +0300

    Добавить тест для редактирования

---
+++ BiogenomWeb.Tests/Unit/Service/Vitamin/UpdateVitaminTest.cs
@@ -0,0 +1,47 @@
+﻿namespace BiogenomWeb.Tests.Unit.Service.Vitamin
+{
+    public class UpdateVitaminTest : BaseUnitTest
+    {
+        [Fact]
+        public async Task UpdateVitaminAsyncTest()
+        {
+            // Arrange
+            int vitaminId = 1;
+            string title = "Update Vitamin";
+
+            // Act & Assert
+            await VitaminService.UpdateVitaminAsync(vitaminId, title);
+        }
+
+        [Fact]
+        public async Task UpdateVitaminAsyncIdZeroTest()
+        {
+            // Arrange
+            int vitaminId = 0;
+            string title = "Update Vitamin";
+
+            // Act & Assert
+            await Assert.ThrowsAsync<InvalidOperationException>(async delegate
+            {
+                await VitaminService.UpdateVitaminAsync(vitaminId, title);
+            });
+        }
+
+        [Fact]
+        public async Task UpdateVitaminAsyncTitleEmptyTest()
+        {
+            // Arrange
+            int vitaminId = 1;
+            string title = string.Empty;
+
+            // Act
+            var ex = await Assert.ThrowsAsync<InvalidOperationException>(async delegate
+            {
+                await VitaminService.UpdateVitaminAsync(vitaminId, title);
+            });
+
+            // Assert
+            Assert.NotEmpty(ex.Message);
+        }
+    }
+}
