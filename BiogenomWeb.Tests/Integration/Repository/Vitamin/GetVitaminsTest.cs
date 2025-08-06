28572ba136d13a6e947a1d26fbf742eb45d74f7e
Hihiz <public@mail.ru>
2025-08-07 01:35:29 +0300

    Добавить тест получения списка витаминов

---
+++ BiogenomWeb.Tests/Integration/Repository/Vitamin/GetVitaminsTest.cs
@@ -0,0 +1,23 @@
+﻿using BiogenomWeb.Domain.Entities;
+
+namespace BiogenomWeb.Tests.Integration.Repository.Vitamin;
+
+public class GetVitaminsTest : IClassFixture<BaseTestContainersIntegration>
+{
+    private readonly BaseTestContainersIntegration _integration;
+
+    public GetVitaminsTest(BaseTestContainersIntegration integration)
+    {
+        _integration = integration;
+    }
+
+    [Fact]
+    public async Task GetVitaminsAsyncTest()
+    {
+        // Arrange && Act    
+        IEnumerable<VitaminEntity> result = await _integration.VitaminRepository!.GetVitaminsAsync();
+
+        // Assert
+        Assert.NotNull(result);
+    }
+}
