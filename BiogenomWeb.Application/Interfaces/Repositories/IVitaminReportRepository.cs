e66c2aeb112379f4de4225084eeda8bc331f9535
Hihiz <public@mail.ru>
2025-08-12 00:13:50 +0300

    Добавить интерфейс репозитория отчета диагностики

---
+++ BiogenomWeb.Application/Interfaces/Repositories/IVitaminReportRepository.cs
@@ -0,0 +1,16 @@
+﻿using BiogenomWeb.Domain.Entities;
+
+namespace BiogenomWeb.Application.Interfaces.Repositories
+{
+    /// <summary>
+    /// Интерфейс репозитория отчета диагностики.
+    /// </summary>
+    public interface IVitaminReportRepository
+    {
+        /// <summary>
+        /// Метод получает отчет последней диагностики.
+        /// </summary>
+        /// <returns>Отчет диагностики.</returns>
+        Task<IEnumerable<VitaminReportEntity>> GetVitaminReportsAsync();     
+    }
+}
