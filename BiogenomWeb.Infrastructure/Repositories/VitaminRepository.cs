4af06a06e1de1e0d36a0973e138916eacf8b6d79
Hihiz <public@mail.ru>
2025-08-12 00:15:55 +0300

    Реализовать метод удаления витамина

---
+++ BiogenomWeb.Infrastructure/Repositories/VitaminRepository.cs
@@ -0,0 +1,45 @@
+﻿using BiogenomWeb.Application.Interfaces.Repositories;
+using BiogenomWeb.Domain.Entities;
+using BiogenomWeb.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace BiogenomWeb.Infrastructure.Repositories
+{
+    /// <summary>
+    /// Класс реализует методы репозитория витаминов.
+    /// </summary>
+    public class VitaminRepository : IVitaminRepository
+    {
+        private readonly ApplicationDbContext _db;
+
+        /// <summary>
+        /// Конструктор.
+        /// </summary>
+        /// <param name="db">Класс контекста.</param>
+        public VitaminRepository(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        #region Публичные методы.
+        /// <inheritdoc />
+        public async Task RemoveVitaminAsync(int vitaminId)
+        {
+            int row = await _db.Vitamins
+                     .Where(v => v.Id == vitaminId)
+                     .ExecuteDeleteAsync();
+
+            if (row <= 0)
+            {
+                throw new InvalidOperationException("Ошибка при удалении витамина. " +
+                                                    $"VitaminId: {vitaminId}.");
+            }
+        }
+
+        #endregion
+
+        #region Приватные методы.
+
+        #endregion        
+    }
+}
