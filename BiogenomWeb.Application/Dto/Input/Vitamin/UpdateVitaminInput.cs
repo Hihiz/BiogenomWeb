d7dbfc048d8d61950177093e8296e718e5337b1e
Hihiz <public@mail.ru>
2025-08-12 00:13:20 +0300

    Добавить входную модель редактирования витамина

---
+++ BiogenomWeb.Application/Dto/Input/Vitamin/UpdateVitaminInput.cs
@@ -0,0 +1,18 @@
+﻿namespace BiogenomWeb.Application.Dto.Input.Vitamin
+{
+    /// <summary>
+    /// Класс входной модели редактирования витамина.
+    /// </summary>
+    public class UpdateVitaminInput
+    {
+        /// <summary>
+        /// Id витамина.
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Название витамина.
+        /// </summary>
+        public string? Title { get; set; }
+    }
+}
